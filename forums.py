a6024ed9e2e239befdd2195f300cbfca7e999147
Levomäki Aapo O <levomaaa@cubbli22-gold.local>
2024-01-31 22:41:32 +0200

    Updated sql-commands to the standard

--- forums.py
+++ forums.py
@@ -7,7 +7,8 @@ from sqlalchemy.sql import text
 import login
 
 def get_forums():
-    sql = "SELECT F.id, F.content, U.name FROM forums F, users U WHERE F.user_id=U.id"
+    sql = "SELECT F.id, F.content, U.name FROM forums F, users U " \
+          "WHERE F.user_id=U.id"
     result = db.session.execute(text(sql))
     return result.fetchall()
 
@@ -15,7 +16,8 @@ def send(content):
     user_id = login.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO forums (content, user_id) VALUES (:content, :user_id)"
+    sql = "INSERT INTO forums (content, user_id) " \
+          "VALUES (:content, :user_id)"
     db.session.execute(text(sql), {"content":content, "user_id":user_id})
     db.session.commit()
     return True
